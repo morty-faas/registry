f005fb0cddeba3e17e0989e52a8b4c691c7dc183
thomasgouveia <gouveia.thomas@outlook.fr>
2023-03-23 08:17:08 +0000

    feat: check for root privileges and add logger configuration

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"os"
+	"os/user"
+
 	"github.com/polyxia-org/morty-registry/internal/registry"
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	logEnvVarKey = "REGISTRY_LOG"
+)
+
 func main() {
-	// log.SetLevel(log.TraceLevel)
+	// Init logger for the app
+	level := log.InfoLevel
+	envLevel := os.Getenv(logEnvVarKey)
+	if envLevel != "" {
+		lvl, err := log.ParseLevel(envLevel)
+		if err != nil {
+			log.Fatalf("failed to parse log level from environment: %v", err)
+		}
+		level = lvl
+	}
+	log.SetLevel(level)
+
+	// Check that the app is running as root
+	// We need to run as root as we make system calls that requires elevated privileges
+	// on the host (such as mount, mkfs.ext4, ...)
+	u, err := user.Current()
+	if err != nil {
+		log.Fatalf("failed to retrieve user: %v", err)
+	}
+
+	// The user is root if the uid is 0
+	if u.Uid != "0" {
+		log.Fatal("registry needs root privileges to work properly")
+	}
+
+	// Run the registry HTTP server
 	reg, err := registry.NewServer()
 	if err != nil {
 		log.Fatalf("failed to initialize the registry: %v", err)
